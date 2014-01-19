faded1784073d64232562e32f389c6030fd19dae
Cyril <cyril@nexu.org>
2014-01-19 13:17:58 +0100

    make some test about localization

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -3,12 +3,31 @@
 class Home extends CI_Controller {
 	
 	//define("NB_ELEM_PAGE",25);
+	public function __construct() {
+		parent::__construct();
+		$this->checkLang();
+	}
 
 
         public function index()
         {
-		$data["title"] = "Title test";
-                $this->load->view('template', $data);
+		//$data["title"] = "Title test";
+                $this->load->view('home/welcome');
         }
+
+	/**
+    	 *i FIXME create a common class to manage language and store selected lan in session
+ 	 */ 
+	private function checkLang() 
+	{
+		$selectedLang = $this->input->post('language');
+		if(!isset($selectedLang)) 
+		{
+			$selectedLang = "english";
+		} 
+
+		$this->lang->load('common', $selectedLang); 
+
+	}
 
 } 
---
+++ application/views/meta_view.php
@@ -0,0 +1,5 @@
+	<meta charset="utf-8">
+	<!--title>Welcome to Irish Guided Tours</title-->
+	<title><?php if(isset($title)) echo $title; else echo lang('welcome') ?></title>
+
+	<link rel="stylesheet" href="<?php echo base_url(); ?>static/style/common.css" type="text/css" />
