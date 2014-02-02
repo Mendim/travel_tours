eb6c8c9ee3d92aef681b045bebe336a91c2adc0e
nanasx <anastasija@nexu.org>
2014-02-02 15:49:29 +0100

    basic russian translations

--- application/language/ru/common_lang.php
+++ application/language/ru/common_lang.php
@@ -1,3 +1,12 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-$lang['welcome'] = "Добро пожаловать в ирландских экскурсовод"; 
+$lang['welcome'] = "Добро пожаловать";
+$lang['home_igt'] = "Экскурсионный тур с гидом по Ирландии";
+$lang['home_offers'] = "Предложения";
+$lang['home_ireland'] = "Ирландия";
+$lang['home_contact_us'] = "О нас";
+$lang['home_langs'] = "Languages";
+$lang['home_langs_ru'] = "Русский";
+$lang['home_langs_en'] = "Английский";
+
+
