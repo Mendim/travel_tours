e747c4c31e0dd9bcbd784b8ac19ade2515194fc5
Cyril <cyril@nexu.org>
2014-02-02 15:15:30 +0100

    Add a super controller to manager lang.

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -1,46 +1,17 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Home extends CI_Controller {
-	
-	//define("NB_ELEM_PAGE",25);
-	public function __construct() {
-		parent::__construct();
-		$this->checkLang();
-	}
+class Home extends MY_Controller {
+
+    //define("NB_ELEM_PAGE",25);
+    public function __construct() {
+        parent::__construct();
+    }
 
 
-        public function index()
-        {
-		//$data["title"] = "Title test";
-                $this->load->view('home/welcome');
-        }
-
-	/**
-    	 *i FIXME create a common class to manage language and store selected lan in session
- 	 */ 
-	private function checkLang() 
-	{
-		$lang = $this->session->userdata('language');
-		$selectedLang = $this->input->get('language');
-		if(!isset($lang)) 
-		{
-			$lang = "en";
-		} 
-
-		$this->lang->load('common', $lang); 
-
-	}
-
-
-	public function changeLang() 
-	{
-		$selectedLang = $this->input->post('language');
-                if(isset($selectedLang))        
-                {
-			$this->session->set_userdata("language", $selectedLang);
-                }
-
-		$this->load->view('home/welcome');
-	}
+    public function index()
+    {
+        //$data["title"] = "Title test";
+        $this->load->view('home/welcome');
+    }
 
 } 
---
+++ application/core/MY_Controller.php
@@ -0,0 +1,34 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class MY_Controller extends CI_Controller {
+	
+	public function __construct() {
+		parent::__construct();
+		$this->checkLang();
+    }
+    
+    
+    public function checkLang() 
+    {
+		$selectedLang = $this->session->userdata('language');
+		if(!isset($selectedLang)) 
+		{
+			$selectedLang = $this->config->item('language');
+		} 
+
+		$this->lang->load('common', $selectedLang); 
+    }
+
+
+    public void setLang() {
+        $selectedLang = $this->input->get('language');
+        if(isset($selectedLang))        
+        {
+            $this->session->set_userdata("language", $selectedLang);
+        }
+
+        $this->load->view('home/welcome');
+    }
+
+}
+
